86c0fd006ec277e8dcf72df8f8b493c4065694ac
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-01-07 18:51:35 +0300

    Create Library class

--- Library.h
+++ Library.h
@@ -1,5 +1,13 @@
-
 #ifndef CPP_PROJECT_LIBRARY_H
 #define CPP_PROJECT_LIBRARY_H
+#include "Book.cpp"
+
+class Library : public LibraryUser, public Book {
+public:
+
+private:
+    array<Book> books;
+    array<LibraryUser> users;
+};
 
 #endif //CPP_PROJECT_LIBRARY_H
