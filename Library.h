7686ea5ede82e3178067aa554d7d287b3d010e1d
Kateridzhe <ekaterina.d.volkova@gmail.com>
2022-01-07 18:58:32 +0300

    Library class change private values list

--- Library.h
+++ Library.h
@@ -1,13 +1,22 @@
 #ifndef CPP_PROJECT_LIBRARY_H
 #define CPP_PROJECT_LIBRARY_H
+
+#include <vector>
 #include "Book.cpp"
 
 class Library : public LibraryUser, public Book {
 public:
+    Library();
+    void addBook();
+    void addUser();
+    void saveRecords();
+    void borrowBook();
+    void returnBook();
+    void printRecords();
 
 private:
-    array<Book> books;
-    array<LibraryUser> users;
+    vector<Book> books;
+    vector<LibraryUser> users;
 };
 
 #endif //CPP_PROJECT_LIBRARY_H
